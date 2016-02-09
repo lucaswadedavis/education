3b32495b9badfa1b28bcecc4bbc2b8ec4545df1d
Luke Davis <luke@peopleofthebit.com>
2016-02-08 16:03:55 -0800

    load sections as either html fragments or markdown.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Antebellum
 
 Antebellum is an essay about the potential of a war to end civilization, collaboratively written and edited on GitHub.
+
+Sections (which can be either html fragments or markdown) are loaded from the /sections directory using the list in /src/sections.json by d3.
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 (function(d3){
-  
+
   var app = {};
-  
+
   app.sectionPaths;
   app.sectionHTMLFragments = [];
   app.sectionsLoaded = 0;
@@ -21,13 +21,30 @@
   };
 
   app.loadHTMLFragment = function (path, index) {
-    d3.html(path, function (data) {
-      app.sectionHTMLFragments[index] = data;
-      app.sectionsLoaded++;
-      if (app.sectionsLoaded >= app.sectionPaths.length) {
-        app.write(app.sectionHTMLFragments);
-      }
-    });
+    var html, md;
+    if (path.indexOf('.html') > -1) {html = true;}
+    if (path.indexOf('.md') > -1) {md = true;}
+    
+    if (html) {
+      d3.html(path, function (data) {
+        app.sectionHTMLFragments[index] = data;
+        app.sectionsLoaded++;
+        if (app.sectionsLoaded >= app.sectionPaths.length) {
+          app.write(app.sectionHTMLFragments);
+        }
+      });
+    } else if (md) {
+      d3.text(path, function (data) {
+        var node = document.createElement('template');
+        node.innerHTML = marked(data);
+        var fragment = node.content;
+        app.sectionHTMLFragments[index] = fragment;
+        app.sectionsLoaded++;
+        if (app.sectionsLoaded >= app.sectionPaths.length) {
+          app.write(app.sectionHTMLFragments);
+        }
+      });
+    }
   };
 
   app.write = function (HTMLFragments) {
@@ -37,7 +54,7 @@
         .appendChild(HTMLFragments[i]);
     }
   };
-  
+
   window.app = app;
 
 })(d3)
